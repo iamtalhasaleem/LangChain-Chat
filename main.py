5ac27a9c88ad5a3c2cc7d7ed48855bf4c6b70742
iamtalhasaleem <iamtalhasaleem911@gmail.com>
2024-09-10 17:56:01 +0500

    memory added

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate 
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from dotenv import load_dotenv
+from langchain.memory import ConversationBufferMemory
 
 load_dotenv()
 
 chat = ChatOpenAI()
 
+memory = ConversationBufferMemory(
+    memory_key = "messages",
+    return_messages = True
+)
 
 prompt = ChatPromptTemplate(
-    input_variables = ["content"],
+    input_variables = ["content", "messages"],
     messages = [
+        MessagesPlaceholder(variable_name = "messages"),
         HumanMessagePromptTemplate.from_template("{content}")
     ]
 )
 
 chain = LLMChain(
     llm = chat,
-    prompt = prompt
+    prompt = prompt,
+    memory = memory
 )
 
 while True:
